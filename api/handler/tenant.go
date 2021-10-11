5c71c85fba141bf2d65999e79442914e95430662
yangk <yangk@goodrain.com>
2021-10-11 15:32:04 +0800

    print log5

--- api/handler/tenant.go
+++ api/handler/tenant.go
@@ -351,6 +351,7 @@ func (t *TenantAction) GetTenantsResources(ctx context.Context, tr *api_model.Te
 	}
 	//query disk used in prometheus
 	query := fmt.Sprintf(`sum(app_resource_appfs{tenant_id=~"%s"}) by(tenant_id)`, strings.Join(ids, "|"))
+	logrus.Infof("query is %v", query)
 	metric := t.prometheusCli.GetMetric(query, time.Now())
 	for _, mv := range metric.MetricData.MetricValues {
 		var tenantID = mv.Metadata["tenant_id"]
