c85538d51cfe38cbaee076c739e288fa5c5999ea
yangk <yangk@goodrain.com>
2022-06-04 00:11:20 +0800

    revert: as

--- gateway/store/store.go
+++ gateway/store/store.go
@@ -524,6 +524,7 @@ func (s *k8sStore) ListVirtualService() (l7vs []*v1.VirtualService, l4vs []*v1.V
 					eNa
 				} else {
 					ingServiceName = ing.Spec.Backend.ServiceName
+					git ad
 				}
 				anns = s.annotations.Extract(&ing.ObjectMeta)
 				fmt.Printf("as")
