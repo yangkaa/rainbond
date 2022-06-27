65999c1d31d7d94c941564ad554b098d8e95add0
yangk <yangk@goodrain.com>
2022-06-27 18:26:13 +0800

    print pull image logInfo3

--- builder/parser/docker_run.go
+++ builder/parser/docker_run.go
@@ -101,7 +101,7 @@ func (d *DockerRunOrImageParse) Parse() ParseErrorList {
 		if strings.Contains(err.Error(), "No such image") {
 			d.errappend(ErrorAndSolve(FatalError, fmt.Sprintf("镜像(%s)不存在", d.image.String()), SolveAdvice("modify_image", "请确认输入镜像名是否正确")))
 		} else {
-			logrus.Infof("pull image %s err %v", err, d.image.String())
+			logrus.Infof("pull image [%s] err [%v]", d.image.String(), err)
 			if d.image.IsOfficial() {
 				d.errappend(ErrorAndSolve(FatalError, fmt.Sprintf("镜像(%s)获取失败,国内访问Docker官方仓库经常不稳定", d.image.String()), SolveAdvice("modify_image", "请确认输入镜像可以正常获取")))
 			} else {
