3a91d937e74a9030c3bb0954ff8072af5a28208b
yangk <yangk@goodrain.com>
2022-06-30 16:10:34 +0800

    print manifest info

--- builder/sources/registry/manifest.go
+++ builder/sources/registry/manifest.go
@@ -39,24 +39,28 @@ func (registry *Registry) Manifest(repository, reference string) (*manifestV1.Si
 	logrus.Infof("registry.manifest.get url=%s repository=%s reference=%s", url, repository, reference)
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
+		logrus.Infof("[http.NewRequest] failed [%v]", err)
 		return nil, err
 	}
 
 	req.Header.Set("Accept", manifestV1.MediaTypeManifest)
 	resp, err := registry.Client.Do(req)
 	if err != nil {
+		logrus.Infof("[registry.Client.Do] failed [%v]", err)
 		return nil, err
 	}
 
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
+		logrus.Infof("[ioutil.ReadAll] failed [%v]", err)
 		return nil, err
 	}
 
 	signedManifest := &manifestV1.SignedManifest{}
 	err = signedManifest.UnmarshalJSON(body)
 	if err != nil {
+		logrus.Infof("[signedManifest.UnmarshalJSON] failed [%v]", err)
 		return nil, err
 	}
 
