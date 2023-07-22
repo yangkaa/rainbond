257616d94e8f19a7109f76394532312374120f4a
zhangsetsail <zqh15131121078@126.com>
2023-07-22 23:34:12 +0800

    feat: gateway adds promethues indicator statement

--- gateway/metric/collectors/nginx_status.go
+++ gateway/metric/collectors/nginx_status.go
@@ -19,9 +19,94 @@
 package collectors
 
 import (
+	"fmt"
+	"github.com/goodrain/rainbond/cmd/gateway/option"
 	"github.com/goodrain/rainbond/gateway/controller/openresty/nginxcmd"
 	"github.com/prometheus/client_golang/prometheus"
+	"io"
+	"net/http"
+	"regexp"
+	"strconv"
 )
+
+var StatusPath = "/nginx_status"
+
+var (
+	ac      = regexp.MustCompile(`Active connections: (\d+)`)
+	sahr    = regexp.MustCompile(`(\d+)\s(\d+)\s(\d+)`)
+	reading = regexp.MustCompile(`Reading: (\d+)`)
+	writing = regexp.MustCompile(`Writing: (\d+)`)
+	waiting = regexp.MustCompile(`Waiting: (\d+)`)
+)
+
+type (
+	basicStatus struct {
+		// Active total number of active connections
+		Active int
+		// Accepted total number of accepted client connections
+		Accepted int
+		// Handled total number of handled connections. Generally, the parameter value is the same as accepts unless some resource limits have been reached (for example, the worker_connections limit).
+		Handled int
+		// Requests total number of client requests.
+		Requests int
+		// Reading current number of connections where nginx is reading the request header.
+		Reading int
+		// Writing current number of connections where nginx is writing the response back to the client.
+		Writing int
+		// Waiting current number of idle client connections waiting for a request.
+		Waiting int
+	}
+)
+
+func toInt(data []string, pos int) int {
+	if len(data) == 0 {
+		return 0
+	}
+	if pos > len(data) {
+		return 0
+	}
+	if v, err := strconv.Atoi(data[pos]); err == nil {
+		return v
+	}
+	return 0
+}
+
+func parse(data string) *basicStatus {
+	acr := ac.FindStringSubmatch(data)
+	sahrr := sahr.FindStringSubmatch(data)
+	readingr := reading.FindStringSubmatch(data)
+	writingr := writing.FindStringSubmatch(data)
+	waitingr := waiting.FindStringSubmatch(data)
+
+	return &basicStatus{
+		toInt(acr, 1),
+		toInt(sahrr, 1),
+		toInt(sahrr, 2),
+		toInt(sahrr, 3),
+		toInt(readingr, 1),
+		toInt(writingr, 1),
+		toInt(waitingr, 1),
+	}
+}
+
+// NewGetStatusRequest creates a new GET request to the internal NGINX status server
+func NewGetStatusRequest(path string, listenPorts option.ListenPorts) (int, []byte, error) {
+	url := fmt.Sprintf("http://127.0.0.1:%v%v", listenPorts.HTTP, path)
+
+	client := http.Client{}
+	res, err := client.Get(url)
+	if err != nil {
+		return 0, nil, err
+	}
+	defer res.Body.Close()
+
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return 0, nil, err
+	}
+
+	return res.StatusCode, data, nil
+}
 
 //NginxCmdMetric -
 type NginxCmdMetric struct {
